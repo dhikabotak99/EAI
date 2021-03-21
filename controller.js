943df4668960234c329fa0b2e835ef480d3190ec
dhikabotak99 <wicakbotak99@gmail.com>
2021-03-21 23:40:27 +0700

    Berhasil menampilkan data buku

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi jalan",res)
 };
+
+// Menampilkan Data Perpus
+exports.tampilSemuaBuku = function(req,res){
+    connection.query('SELECT * FROM buku', function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
+};
