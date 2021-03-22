cd6097aa4fecf6901c193f466439dc8ea9872877
dhikabotak99 <wicakbotak99@gmail.com>
2021-03-22 09:04:39 +0700

    Membuat method update

--- controller.js
+++ controller.js
@@ -3,6 +3,7 @@
 var response = require('./res');
 var connection = require('./koneksi');
 const conn = require('./koneksi');
+const { connect } = require('./koneksi');
 
 exports.index = function(req, res){
     response.ok("Aplikasi jalan",res)
@@ -32,6 +33,24 @@ exports.tambahBuku = function(req,res){
             console.log(error);
         }else{
             response.ok("Berhasil menambah data!",res)
+        }
+    });
+};
+
+// Mengubah data berdasarkan id
+exports.ubahBuku = function(req,res){
+    var id = req.body.id;
+    var judul = req.body.judul;
+    var penulis = req.body.penulis;
+    var stock = req.body.stock;
+    var kondisi = req.body.kondisi;
+
+    connection.query('UPDATE buku SET judul=?, penulis=?, stock=?, kondisi=? WHERE id=?', [judul, penulis, stock, kondisi, id],
+    function (error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok("Berhasil mengubah data!",res)
         }
     });
 };
--- routes.js
+++ routes.js
@@ -11,4 +11,7 @@ module.exports = function(app) {
 
     app.route('/tambah')
     .post(jsonku.tambahBuku);
+
+    app.route('/ubah')
+    .put(jsonku.ubahBuku);
 }
