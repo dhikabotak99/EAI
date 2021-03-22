ef0e5cee53e7b68e8b04aa84822be10410848f36
dhikabotak99 <wicakbotak99@gmail.com>
2021-03-22 08:52:46 +0700

    Membuat method tambah

--- controller.js
+++ controller.js
@@ -2,18 +2,36 @@
 
 var response = require('./res');
 var connection = require('./koneksi');
+const conn = require('./koneksi');
 
 exports.index = function(req, res){
     response.ok("Aplikasi jalan",res)
 };
 
-// Menampilkan Data Perpus
+// Menampilkan Data Buku
 exports.tampilSemuaBuku = function(req,res){
     connection.query('SELECT * FROM buku', function(error, rows, fields){
         if(error){
             console.log(error);
         }else{
             response.ok(rows, res)
+        }
+    });
+};
+
+// Menambah Data Buku
+exports.tambahBuku = function(req,res){
+    var judul = req.body.judul;
+    var penulis = req.body.penulis;
+    var stock = req.body.stock;
+    var kondisi = 'Tersedia';
+
+    connection.query('INSERT INTO buku (judul, penulis, stock, kondisi) VALUES(?,?,?,?)',[judul,penulis,stock,kondisi],
+    function (error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok("Berhasil menambah data!",res)
         }
     });
 };
--- routes.js
+++ routes.js
@@ -8,4 +8,7 @@ module.exports = function(app) {
 
     app.route('/tampil')
     .get(jsonku.tampilSemuaBuku);
+
+    app.route('/tambah')
+    .post(jsonku.tambahBuku);
 }
